1a20e05d6c8d9cc8e23ba46129c40feb2cbf375f
Leonel Mora <viefmoon@gmail.com>
2021-11-09 17:22:14 -0600

    9 nov

--- src/main.cpp
+++ src/main.cpp
@@ -24,15 +24,27 @@ uint8_t MastereNewMACAddress[] = {0xAA, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 
 struct struct_message {
     uint8_t id; // must be unique for each sender board
-    char uuid_[8];
     uint8_t rssi;
+    char uuid_[9];
 };
 struct_message msg;
 struct_message aux;
 
 uint8_t rssi[3][5];
 
+float prom[3];
+
 uint8_t cont[3] = {0, 0, 0};
+bool flag_cont[3] = {0, 0, 0};
+
+
+float average(size_t id){
+    float suma = 0;
+    for (int x = 0; x < 5; x++){
+        suma = suma + rssi[id][x];
+    }
+    return suma / 5;
+}
 
 
 void OnDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len) {
@@ -41,26 +53,43 @@ void OnDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len) 
   snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
   Serial.println(macStr);
   memcpy(&msg, incomingData, sizeof(msg));
+  for (size_t i = 0; i < 9; i++){
+      Serial.print(msg.uuid_[i]);
+  }
+   for (size_t i = 0; i < 9; i++){
+      Serial.print(aux.uuid_[i]);
+  }
+  Serial.print("  RSSI: ");
+  Serial.println(msg.rssi);
   Serial.printf("Board ID %u: %u bytes\n", msg.id, len);
+
 
   if(aux.uuid_[0]==0){
     //Se agrega un uuid para empezar a comprobar este
-    for (size_t i = 0; i <= 8; i++){
-      aux.uuid_[i]  = msg.uuid_[i];
-    }
+    strncpy(aux.uuid_, msg.uuid_, sizeof (msg.uuid_));
     Serial.print("Searching for UUID: ");
     for (size_t i = 0; i < 8; i++){
       Serial.print(aux.uuid_[i]);
     }
     Serial.println("");
   }
-      for (size_t i = 0; i < 8; i++){
-      Serial.print(msg.uuid_[i]);
-    }
-    Serial.println("");
+
   if(strcmp(aux.uuid_, msg.uuid_) == 0){
+    Serial.println("same");
     if(cont[msg.id]==5){
       cont[msg.id]=0;
+      flag_cont[msg.id] = 1;
+    }
+    if(flag_cont[0]&&flag_cont[1]&&flag_cont[2]){
+      cont[msg.id]=0;
+      flag_cont[msg.id] = 1;
+      Serial.print("Average RSSI of slaves ");
+      for (size_t i = 0; i < 3; i++){
+        prom[i] = average(i);
+        Serial.print(i);
+        Serial.print(" : ");
+        Serial.println(prom[i]);
+      }
     }
     rssi[msg.id][cont[msg.id]] = msg.rssi;
     Serial.print("Board ID: ");
